970696cc0781300038d41be4e3d77e7506f5f65d
TheGameFreak720 <luis.alvarez@commithub.com>
2018-02-27 23:46:25 -0500

    Bot can get tweets and post a tweet

--- bot.js
+++ bot.js
@@ -8,4 +8,20 @@ var T = new Twit({
     access_token_secret: config.accessSecret
 });
 
+//  search twitter for all tweets containing the word 'coding'
+
+T.get('search/tweets', { q: 'coding', count: 2 }, function(err, data, response) {
+    let tweets = data.statuses;
+
+    for (let i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
+});
+
+//  tweet a hello message from the bot
+
+T.post('statuses/update', { status: 'hello earthlings this is microbot! (testing a bot using twitter api with node)' }, function(err, data, response) {
+    console.log(data)
+});
+
 console.log('The bot is starting');
