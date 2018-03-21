1736a875c44a4871a0003fd883e9dae6d3874b46
TheGameFreak720 <luis.alvarez@commithub.com>
2018-03-20 21:06:47 -0400

    It will ping marshy twitch account every 10 minutes and if it finds it online it will post a tweet of the game he is playing with a link to his channel

--- bot.js
+++ bot.js
@@ -47,9 +47,16 @@ function postTweetMarshy() {
     let fetchTwitch = setInterval(function () {
         request.get(options, function (error, response, body) {
             let json = JSON.parse(body);
+
+            var tweet = {
+                status: json.stream.channel.display_name + ' is playing ' + json.stream.channel.game
+                + ' follow him at ' + json.stream.channel.url
+            };
+
             if (json.stream !== null) {
-                console.log(json.stream.channel.display_name + ' is playing ' + json.stream.channel.game
-                    + ' follow him at ' + json.stream.channel.url);
+                T.post('statuses/update', tweet, function (err, data, response) {
+                    console.log(data)
+                });
                 clearInterval(fetchTwitch);
                 let wait = setInterval(function() {
                     clearInterval(wait);
@@ -57,10 +64,10 @@ function postTweetMarshy() {
                 }, 86400000); // 24 hours
             }
         });
-    }, 1800000); //30 minutes
+    }, 600000); //10 minutes
 
     // 1s = 1000ms and 1m = 60s, so 60 * 1000 = 60000ms
-    // 60s * 30 = 1,800s so 1,800 * 1000 = 1,800,000ms
+    // 60s * 30 = 600s so 600 * 1000 = 600,000ms
     // 60s * 60 = 3,600 * 24 hr = 86,400 so 86,400 * 1000 = 86,400,000ms
 }
 
