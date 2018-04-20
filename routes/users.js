edd58df359a43767167aabd21e04a8d039747172
TheGameFreak720 <luis.alvarez@commithub.com>
2018-04-19 22:59:32 -0400

    added user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.get('/login', function(req, res) {
    res.render('login', {
        title:'Login'
@@ -12,5 +14,45 @@ router.get('/register', function(req, res) {
         title:'Register'
     });
 });
+
+router.post('/register', function(req, res) {
+    let name = req.body.name;
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+    let password2 = req.body.password2;
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('register', {
+            title:'Register',
+            errors:errors
+        });
+    } else {
+        let newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+
+        User.createUser(newUser,  function(err, user) {
+           if (err) throw err;
+           console.log(user);
+        });
+
+        req.flash('success', 'You are registered and can now login');
+        res.redirect('/users/login');
+    }
+});
+
 
 module.exports = router;
