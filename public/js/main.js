2daae1b5005503aa06e77ecf05b0b65232b89b49
TheGameFreak720 <luis.alvarez@commithub.com>
2018-04-24 21:31:33 -0400

    added post routes

--- public/js/main.js
+++ public/js/main.js
@@ -31,5 +31,24 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('.delete-post').on('click', function(e) {
+        $target = $(e.target);
+        const id = $target.attr('data-id');
+        $.ajax({
+            type: 'DELETE',
+            url: '/post/' + id,
+            success: function(response) {
+                alert('Deleting Post');
+                window.location.href='/';
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+    });
 });
+
+
+
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,138 @@
 const express = require('express');
 const router = express.Router();
 
+//Bring in Article Model
+let Post = require('../models/post');
+
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        req.flash('danger','You need to sign in for access to this page');
+        res.redirect('/users/login');
+    }
+}
+
+///Article Routes
+router.get('/', ensureAuthenticated, function(req, res) {
+    Post.find({}, function(err, posts) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('posts', {
+                title:'Posts',
+                posts: posts
+            });
+        }
+    });
+});
+
+//Load Edit Form
+router.get('/edit/:id', ensureAuthenticated, function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        res.render('edit_post', {
+            title: 'Edit Post',
+            post: post
+        });
+    });
+});
+
 //Add Route
 router.get('/add', function(req, res) {
     res.render('add_post', {
         title: 'Add Post'
     });
 });
+
+//Add Submit Post Route
+router.post('/add', function(req, res) {
+    req.checkBody('body', 'Body is required!').notEmpty();
+    req.checkBody('link', 'Link is required!').notEmpty();
+    req.checkBody('date', 'Date is required!').notEmpty();
+
+    //Get errors
+    let errors = req.validationErrors();
+
+    if (errors) {
+        res.render('add_post', {
+            title:'Add Post',
+            errors:errors
+        });
+    } else {
+        let post = new Post();
+        post.body = req.body.body;
+        post.link = req.body.link;
+        post.date = req.body.date;
+
+        post.save(function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                req.flash('success', 'Post Added');
+                res.redirect('/posts');
+            }
+        });
+    }
+});
+
+//Update Submit Post Route
+router.post('/edit/:id', function(req, res) {
+    req.checkBody('body', 'Body is required!').notEmpty();
+    req.checkBody('link', 'Link is required!').notEmpty();
+    req.checkBody('date', 'Date is required!').notEmpty();
+
+    //Get errors
+    let errors = req.validationErrors();
+
+    if (errors) {
+        Post.findById(req.params.id, function(err, post) {
+            res.render('edit_post', {
+                title: 'Edit Post',
+                post: post,
+                errors: errors
+            });
+        });
+    } else {
+        let post = {};
+        post.body = req.body.body;
+        post.link = req.body.link;
+        post.date = req.body.date;
+
+        let query = {_id: req.params.id};
+
+        Post.update(query, post, function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                req.flash('success', 'Post Updated');
+                res.redirect('/posts');
+            }
+        });
+    }
+});
+
+router.delete('/:id', function(req,res) {
+
+    let query = {_id:req.params.id};
+
+    Post.remove(query, function(err) {
+        if(err) {
+            console.log(err);
+        }
+        res.send('Success');
+    });
+});
+
+//Get single Article
+router.get('/:id', ensureAuthenticated, function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        res.render('post', {
+            post: post
+        });
+    });
+});
+
 
 module.exports = router;
