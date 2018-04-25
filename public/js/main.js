4774c524aa7df9675523077e0999f8f2b44ef7bc
TheGameFreak720 <luis.alvarez@commithub.com>
2018-04-24 23:44:37 -0400

    made passwords hidden and if you click a box they can be seen

--- public/js/main.js
+++ public/js/main.js
@@ -46,6 +46,26 @@ $(document).ready(function() {
     });
 });
 
+function showPassword() {
+    var x = document.getElementById("password");
+
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showPassword2() {
+    var x = document.getElementById("password2");
+
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
 
 
 
